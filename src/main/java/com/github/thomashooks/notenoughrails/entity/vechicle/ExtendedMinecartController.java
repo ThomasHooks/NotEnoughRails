30f4228968322a05964a16a54dbf1ecbfa80a0b2
ThomasHooks <kilroy790@gmail.com>
2026-03-29 11:22:09 -0400

    Fixed defect that would cause a crash if minecarts went off rail

--- src/main/java/com/github/thomashooks/notenoughrails/entity/vechicle/ExtendedMinecartController.java
+++ src/main/java/com/github/thomashooks/notenoughrails/entity/vechicle/ExtendedMinecartController.java
@@ -1,3 +1,18 @@
+/*
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.github.thomashooks.notenoughrails.entity.vechicle;
 
 import com.mojang.datafixers.util.Pair;
@@ -231,8 +246,10 @@ public class ExtendedMinecartController extends DefaultMinecartController {
     public double getMaxSpeed(ServerWorld world) {
         BlockPos pos = this.minecart.getRailOrMinecartPos();
         BlockState state = world.getBlockState(pos);
-        AbstractRailBlock railBlock = (AbstractRailBlock) state.getBlock();
-        return railBlock.notEnoughRails$getMaxSpeed(state, pos, this.minecart);
+        if (state.getBlock() instanceof AbstractRailBlock railBlock) {
+            return railBlock.notEnoughRails$getMaxSpeed(state, pos, this.minecart);
+        }
+        return super.getMaxSpeed(world);
     }
 
     protected Vec3d applySlowdown(@NonNull Vec3d velocityIn) {
